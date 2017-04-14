376aa34cce9ec762c1934755fb6a0a3a0254613e
Adrianna <0935007@hr.nl>
2017-04-14 19:23:55 +0200

    ok map is soort van gelukt, je moet pip installen geojson

---
+++ Roffa_map_test.py
@@ -0,0 +1,29 @@
+
+#pip install geoplotlib
+import geoplotlib
+from geoplotlib.utils import BoundingBox
+from geoplotlib.colors import ColorMap
+from geoplotlib.utils import json
+import json
+import requests
+
+
+
+# find the unemployment rate for the selected county, and convert it to color
+def get_color(properties):
+    key = str(int(properties['jaar'])) + properties['acode']
+    if key in Roffa:
+        return cmap.to_color(Roffa.get(key), .15, 'lin')
+    else:
+        return [0, 0, 0, 0]
+
+
+with open('Rotterdam_map.json','r') as fin:
+    Roffa = json.load(fin)
+    print (Roffa)
+
+cmap = ColorMap('Reds', alpha=255, levels=10)
+geoplotlib.geojson('Rotterdam_map.json', fill=True, color=[255, 255, 255, 64], f_tooltip=lambda properties: properties['jaar'])
+geoplotlib.geojson('Rotterdam_map.json', fill=False, color=[255, 255, 255, 64])
+# geoplotlib.set_bbox(BoundingBox.east)
+geoplotlib.show()
