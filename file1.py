4d513d2a937938f2ebf8d401b2403c75bd477916
nikkichi <0930364@hr.nl>
2017-04-18 11:41:22 +0200

    Database queries

--- file1.py
+++ file1.py
@@ -5,8 +5,11 @@ connect = sqlite3.connect('test.db')
 c = connect.cursor()
 
 def SelectDB():
-    c.execute('SELECT * FROM Straatroven')
-    data = c.fetchall()
-    print(data)
+    c.execute("SELECT R.name, R.latitude, R.longitude, S.Straat FROM Straatroven S, RET R WHERE S.leeftijdslachtoffer = 18-24 AND S.postcode = R.postcode AND S.dagdeel = '00:00-05:59' AND S.Opgelost = 'J' AND S.pogingtotmoord = 'N'")
+#    data = c.fetchall()
+    #row so data prints in rows
+    for row in c.fetchall():
+        print(row)
+#    print(data)
 
 SelectDB()
