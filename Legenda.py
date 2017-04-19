9c7620015c246de9267c95e072b13a69280ea2e3
Akin Uzel <taigakin@hotmail.com>
2017-04-19 16:43:10 +0200

    added legend

--- Legenda.py
+++ Legenda.py
@@ -6,7 +6,7 @@ from PyQt5.QtGui import QPixmap
 class legenda(QtWidgets.QWidget):
     def runlegenda(self):
         self.pic = QLabel()
-        self.pic.setPixmap(QPixmap('example4.png'))
+        self.pic.setPixmap(QPixmap('Legenda_voor_map.png'))
         self.pic.show()
         app.exec_()
 
