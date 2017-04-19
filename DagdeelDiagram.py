03393cebbbc1df1465e3b2ef9f5f4725e59c310e
Mediamarkt <0925178@hr.nl>
2017-04-19 15:22:57 +0200

    the finished bar chart for the robberies by time of day

--- DagdeelDiagram.py
+++ DagdeelDiagram.py
@@ -4,47 +4,57 @@ import sqlite3
 connect = sqlite3.connect('test.db')
 c = connect.cursor()
 
-# = ("SELECT count (*) FROM Straatroven WHERE dagdeel = '00:00-05:59' AND leeftijdslachtoffer = '18-24'")
-#data1 = ("SELECT count (*) FROM Straatroven WHERE dagdeel = '06:00-11:59' AND leeftijdslachtoffer = '18-24'")
-#data2 = ("SELECT count (*) FROM Straatroven WHERE dagdeel = '12:00-17:59' AND leeftijdslachtoffer = '18-24'")
-#data3= ("SELECT count (*) FROM Straatroven WHERE dagdeel = '18:00-23:59' AND leeftijdslachtoffer = '18-24'")
 
-def listfix():
-    c = "SELECT COUNT(*) FROM Straatroven WHERE leeftijdslachtoffer = '18-24' GROUP BY dagdeel"
-    args = [[0]]
-    c.execute(c, args)
-    data = c.fetchall()
-    for dagdeel in data:
-        one = data[0]
-        # two = dagdeel[1]
-        # three = dagdeel[2]
-        # four = dagdeel[3]
-        print(data)
+def dagdeel1(): #function for picking up the first value from our database
+    c.execute ("SELECT count (*) FROM Straatroven WHERE dagdeel = '00:00-05:59' AND leeftijdslachtoffer = '18-24'")
+    rows = c.fetchall()
+    for r in rows:
+        return (int(r[0]))
 
-listfix()
-c.close
-connect.close()
 
-# x = [2]
-# y = yo
+def dagdeel2(): #function for picking up the second value from our database
+    c.execute ("SELECT count (*) FROM Straatroven WHERE dagdeel = '06:00-11:59' AND leeftijdslachtoffer = '18-24'")
+    rows = c.fetchall()
+    for r in rows:
+        return (int(r[0]))
 
-#x1 = [4]
-#y1 = data1
+def dagdeel3(): #function for picking up the third value from our database
+    c.execute ("SELECT count (*) FROM Straatroven WHERE dagdeel = '12:00-17:59' AND leeftijdslachtoffer = '18-24'")
+    rows = c.fetchall()
+    for r in rows:
+        return (int(r[0]))
 
-#x2 = [6]
-#y2 = data2
+def dagdeel4(): #function for picking up the fourth value from our database
+    c.execute ("SELECT count (*) FROM Straatroven WHERE dagdeel = '18:00-23:59' AND leeftijdslachtoffer = '18-24'")
+    rows = c.fetchall()
+    for r in rows:
+        return (int(r[0]))
 
-#x3 = [8]
-#y3 = data3
 
-# plt.bar(x,y)
-# # plt.bar(x1,y1)
-# # plt.bar(x2,y2)
-# # plt.bar(x3,y3)
-#
-#
-# plt.xlabel('dagdeel')
-# plt.ylabel('aantal')
-# plt.title('Straatroven per dagdeel \n Leeftijdsgroep 18-24 jaar ')
-# plt.legend()
-# plt.show()
+x = [2]
+y = dagdeel1() #adding values
+
+x1 = [4]
+y1 = dagdeel2() #adding values
+
+x2 = [6]
+y2 = dagdeel3() #adding values
+
+x3 = [8]
+y3 = dagdeel4() #adding values
+
+plt.bar(x,y)
+plt.bar(x1,y1)
+plt.bar(x2,y2)
+plt.bar(x3,y3)
+
+
+plt.legend(['00:00-05:59', '06:00-11:59', '12:00-17:59', '18:00-23:59'], loc='upper left') #legend with 4 values (the 4 daytimes)
+plt.grid()
+
+plt.xlabel('time of day')
+plt.ylabel('amount of roberries')
+plt.title('robberies by times of the day \n age group 18-24 years')
+plt.legend()
+plt.show()
+
